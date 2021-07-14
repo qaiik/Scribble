a89825e0cb95096a399b00ac05f074075c6b9e38
qaiik <82181231+qaiik@users.noreply.github.com>
2021-07-13 20:10:45 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ import time
 
 words = [_.strip('\n') for _ in open("words.txt").readlines()]
 
-time.sleep(3)
+length = int(input("Length: "))
+
+
+time.sleep(2)
 for word in words:
-  pg.typewrite(word)
-  time.sleep(2.5)
+  if len(word) == length:
+    pg.typewrite(word, interval=0.00)
+    pg.press("enter")
+    time.sleep(0.7)
